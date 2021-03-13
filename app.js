167b30f15db300bfdd661480cd11a2ff27e31cec
khjhs101198 <58767897+khjhs101198@users.noreply.github.com>
2021-03-13 12:33:33 +0800

    add procfile

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const { auth } = require('express-openid-connect');
 const app = express();
@@ -8,9 +9,9 @@ app.use(express.json(), express.urlencoded({extended: false}));
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'wK2axgz8XqZsAN70z2cNFA5QxrusjCMYSAIYvBcnWkAAZQnXOa5tNIOuFhPvKGQz',
-  baseURL: 'http://localhost:3000',
-  clientID: 'sYUw4oI4tmM7Zz5a0YyfquUyecScOau6',
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: 'https://myTestAPI.herokuapp.com',
+  clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: 'https://dev-j5dnvaff.us.auth0.com'
 };
 app.use(auth(config));
@@ -19,11 +20,11 @@ app.get("/", (req, res) => {
   res.send("Home page");
 });
 
-app.get("/callback", (req, res) => {
+app.get("/api/callback", (req, res) => {
   res.send("Callback page");
 });
 
-app.listen(3000, function(err){
+app.listen(process.env.PORT||3000, function(err){
   if(err) throw err;
-  console.log(`Server is listened on port: 5000`);
+  console.log(`Server started`);
 });
